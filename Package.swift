8f3f6b83319445e9f5f8d43c3f3879ac289eca38
anandyn2002 <45676373+anandyn2002@users.noreply.github.com>
2023-10-16 09:26:50 +0530

    updated package

--- Package.swift
+++ Package.swift
@@ -12,15 +12,15 @@ let package = Package(
             targets: ["PMPComponents"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/freshOS/Stevia", exact: "5.1.2"),
+        .package(url: "https://github.com/anandyn02/PMP_Component", exact: "1.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PMPComponents",
-            dependencies: [],path: "Sources"),
+            dependencies: ["Stevia", "PMP_Component"],path: "Sources"),
         .testTarget(
             name: "PMPComponentsTests",
             dependencies: ["PMPComponents"]),
---
+++ Sources/PMPComponents/ReadTimeView.swift
@@ -0,0 +1,76 @@
+//
+//  ReadTimeView.swift
+//  PenMyPlan
+//
+//  Created by Anand  on 13/10/23.
+//  Copyright © 2023 PenMyPlan. All rights reserved.
+//
+
+import UIKit
+import PMP_Component
+import Stevia
+
+class ReadTimeView: UIView {
+
+    private lazy var postImageView: UIImageView = {
+        let imageView = IOComponent.createImageView(radius: 0, bgColor: .clear)
+        imageView.width(18)
+        imageView.height(18)
+        return imageView
+    }()
+    
+    private lazy var readLbl = IOComponent.createLabel(text: "Read Time", font: UIFont.systemFont(ofSize: 12), color: .black)
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    var readFont: UIFont? {
+        didSet {
+            readLbl.font = readFont
+        }
+    }
+    
+    var readColor: UIColor? {
+        didSet {
+            readLbl.textColor = readColor
+        }
+    }
+    var readText: String = "" {
+        didSet {
+            readLbl.text = readText
+        }
+    }
+    
+    var readIcon: UIImage? {
+        didSet {
+            postImageView.image = readIcon
+        }
+    }
+    
+    func configure(){
+        
+        readLbl.text = "Read Time: 2 mins"
+       // postImageView.image = UIImage(named: "eye_new")
+        
+        self.subviews {
+            postImageView
+            readLbl
+        }
+        self.layout {
+            10
+            |-10-postImageView-10-readLbl-10-|
+            10
+        }
+        
+        self.layer.cornerRadius = 8
+        self.layer.borderColor = UIColor(red: 49.0/255.0, green: 48.0/255.0, blue: 51.0/255.0, alpha: 1.0).cgColor
+        self.layer.borderWidth = 1
+    }
+
+}
