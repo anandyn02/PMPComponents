96ba885ab7f9e15efcfd4b4bee4ba08852f16d88
anandyn2002 <45676373+anandyn2002@users.noreply.github.com>
2023-10-22 12:28:03 +0530

    Update ChatInfo.swift

--- Sources/PMPComponents/ChatInfo.swift
+++ Sources/PMPComponents/ChatInfo.swift
@@ -11,7 +11,16 @@ public struct ChatInfo {
     public var type: ChatInfoCellType = .none
     public var imageName: String?
     public var imageUrl: String?
-    public var chatId: String?
+    public var infoId: String?
     public var name: String?
     public var subName: String?
+    
+    public init(type: ChatInfoCellType, imageName: String?, imageUrl: String?, userId: String?, name: String?, subName: String?){
+        self.type = type
+        self.imageName = imageName
+        self.imageUrl = imageUrl
+        self.infoId = userId
+        self.name = name
+        self.subName = subName
+    }
 }
