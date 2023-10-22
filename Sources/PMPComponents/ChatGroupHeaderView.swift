9f7d8e3b595f8aa3a6e1ae8fbec3670fc010b84d
anandyn2002 <45676373+anandyn2002@users.noreply.github.com>
2023-10-22 09:22:25 +0530

    Create ChatGroupHeaderView.swift

---
+++ Sources/PMPComponents/ChatGroupHeaderView.swift
@@ -0,0 +1,51 @@
+//
+//  ChatGroupHeaderView.swift
+//  
+//
+//  Created by Anand  on 22/10/23.
+//
+
+import UIKit
+import PMP_Component
+import Stevia
+
+public class ChatGroupHeaderView: UIView {
+    
+    public let label = IOComponent.createLabel(text: "", font: .systemFont(ofSize: 14), color: .black)
+    public let subLabel = IOComponent.createLabel(text: "", font: .systemFont(ofSize: 12), color: .lightGray)
+    public let avatar = Avatar()
+    
+    public override init(frame: CGRect) {
+         super.init(frame: frame)
+         configure()
+     }
+    
+     required init?(coder: NSCoder) {
+         fatalError("init(coder:) has not been implemented")
+     }
+    
+    func configure(){
+        
+        subviews {
+            avatar
+            label
+            subLabel
+        }
+        
+        avatar.width(80).height(80)
+        
+        layout {
+            20
+            avatar.centerHorizontally()
+            10
+            |-20-label-20-|
+            4
+            |-20-subLabel-20-|
+            20
+            
+        }
+        
+    }
+   
+
+}
