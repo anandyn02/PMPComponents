85ff0f3cd732b00cd08ce5e2ee8df1be9a3835db
anandyn2002 <45676373+anandyn2002@users.noreply.github.com>
2023-10-22 12:16:48 +0530

    Create ChatInfo.swift

---
+++ Sources/PMPComponents/ChatInfo.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Anand  on 22/10/23.
+//
+
+import Foundation
+
+public struct ChatInfo {
+    let type: ChatInfoCellType
+    var imageName: String?
+    var imageUrl: String?
+    var chatId: String?
+    var name: String?
+    var subName: String?
+}
