78f31fc8afc36755d6458f77aee244444b65a4b9
anandyn2002 <45676373+anandyn2002@users.noreply.github.com>
2023-10-30 10:38:57 +0530

    Update PMPCrypto.swift

--- Sources/PMPComponents/PMPCrypto.swift
+++ Sources/PMPComponents/PMPCrypto.swift
@@ -10,13 +10,13 @@ import RNCryptor
 
 public class PMPCrypto {
     
-    public func encryptMessage(message: String, encryptionKey: String) throws -> String {
+    public class func encryptMessage(message: String, encryptionKey: String) throws -> String {
           let messageData = message.data(using: .utf8)!
           let cipherData = RNCryptor.encrypt(data: messageData, withPassword: encryptionKey)
           return cipherData.base64EncodedString()
       }
 
-    public func decryptMessage(encryptedMessage: String, encryptionKey: String) throws -> String {
+    public class func decryptMessage(encryptedMessage: String, encryptionKey: String) throws -> String {
 
           let encryptedData = Data.init(base64Encoded: encryptedMessage)!
           let decryptedData = try RNCryptor.decrypt(data: encryptedData, withPassword: encryptionKey)
