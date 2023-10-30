9d8c6ff3847f3c7a4c321f9a2891047b05ea0414
anandyn2002 <45676373+anandyn2002@users.noreply.github.com>
2023-10-30 10:36:32 +0530

    Crypto files added

--- Package.swift
+++ Package.swift
@@ -15,14 +15,15 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/freshOS/Stevia", exact: "5.1.2"),
         .package(url: "https://github.com/anandyn02/PMP_Component", exact: "1.0.1"),
-        .package(url:"https://github.com/onevcat/Kingfisher", exact: "7.9.1")
+        .package(url:"https://github.com/onevcat/Kingfisher", exact: "7.9.1"),
+        .package(url:"https://github.com/RNCryptor/RNCryptor", exact: "5.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PMPComponents",
-            dependencies: ["Stevia", "PMP_Component", "Kingfisher"],path: "Sources"),
+            dependencies: ["Stevia", "PMP_Component", "Kingfisher", "RNCryptor"],path: "Sources"),
         .testTarget(
             name: "PMPComponentsTests",
             dependencies: ["PMPComponents"]),
---
+++ Sources/PMPComponents/PMPCrypto.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  
+//
+//  Created by Anand  on 30/10/23.
+//
+
+import Foundation
+import RNCryptor
+
+public class PMPCrypto {
+    
+    public func encryptMessage(message: String, encryptionKey: String) throws -> String {
+          let messageData = message.data(using: .utf8)!
+          let cipherData = RNCryptor.encrypt(data: messageData, withPassword: encryptionKey)
+          return cipherData.base64EncodedString()
+      }
+
+    public func decryptMessage(encryptedMessage: String, encryptionKey: String) throws -> String {
+
+          let encryptedData = Data.init(base64Encoded: encryptedMessage)!
+          let decryptedData = try RNCryptor.decrypt(data: encryptedData, withPassword: encryptionKey)
+          let decryptedString = String(data: decryptedData, encoding: .utf8)!
+
+          return decryptedString
+      }
+}
