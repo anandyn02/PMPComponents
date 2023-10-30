179a92f47c4c28768b74239c2d2dbcf81d586339
anandyn2002 <45676373+anandyn2002@users.noreply.github.com>
2023-10-30 10:18:57 +0530

    Update QRGenerator.swift

--- Sources/PMPComponents/QRGenerator.swift
+++ Sources/PMPComponents/QRGenerator.swift
@@ -9,13 +9,20 @@ import UIKit
 
 public class QRGenerator {
     
-   public class func generateQRCode(from string: String) -> UIImage? {
+    public class func generateQRCode(from string: String) -> UIImage? {
         
         let data = string.data(using: String.Encoding.ascii)
         if let QRFilter = CIFilter(name: "CIQRCodeGenerator") {
             QRFilter.setValue(data, forKey: "inputMessage")
-            guard let QRImage = QRFilter.outputImage else {return nil}
-            return UIImage(ciImage: QRImage)
+            
+            let transform = CGAffineTransform(scaleX: 3, y: 3)
+            
+            if let QRImage = QRFilter.outputImage?.transformed(by: transform) {
+                return UIImage(ciImage: QRImage)
+            }
+            
+            //            guard let QRImage = QRFilter.outputImage else {return nil}
+            //            return UIImage(ciImage: QRImage)
         }
         return nil
     }
