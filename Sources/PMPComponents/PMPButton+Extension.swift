d239f5df8a45f1d16e604a3a903c24b6da4585e3
anandyn2002 <45676373+anandyn2002@users.noreply.github.com>
2023-11-21 14:29:44 +0530

    Update PMPButton+Extension.swift

--- Sources/PMPComponents/PMPButton+Extension.swift
+++ Sources/PMPComponents/PMPButton+Extension.swift
@@ -23,3 +23,28 @@ public extension UIButton {
         }
     }
 }
+
+
+@IBDesignable
+public class LeftAlignedIconButton: UIButton {
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        contentHorizontalAlignment = .left
+        let availableSpace = bounds.inset(by: contentEdgeInsets)
+        let availableWidth = availableSpace.width - imageEdgeInsets.right - (imageView?.frame.width ?? 0) - (titleLabel?.frame.width ?? 0)
+        titleEdgeInsets = UIEdgeInsets(top: 0, left: availableWidth / 2, bottom: 0, right: 0)
+    }
+}
+
+
+@IBDesignable
+public class RightAlignedIconButton: UIButton {
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        semanticContentAttribute = .forceRightToLeft
+        contentHorizontalAlignment = .right
+        let availableSpace = bounds.inset(by: contentEdgeInsets)
+        let availableWidth = availableSpace.width - imageEdgeInsets.left - (imageView?.frame.width ?? 0) - (titleLabel?.frame.width ?? 0)
+        titleEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: availableWidth / 2)
+    }
+}
