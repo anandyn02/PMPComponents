34a65f7402b17da6067865b02fb74c3f526a495b
anandyn2002 <45676373+anandyn2002@users.noreply.github.com>
2023-12-05 15:30:11 +0530

    Update PlanLocationTableViewCell.swift

--- Sources/PMPComponents/PlanLocationTableViewCell.swift
+++ Sources/PMPComponents/PlanLocationTableViewCell.swift
@@ -50,7 +50,7 @@ public class PlanLocationTableViewCell: UITableViewCell {
         
         layout {
             10
-            |-20-label-20-| ~ 22
+            |-20-label-20-|
             10
             |-20-stack-20-|
             10
