e5327a0b9889505a38dc9f5ac7445ae0197c3b60
anandyn2002 <45676373+anandyn2002@users.noreply.github.com>
2024-01-29 13:35:56 +0530

    Update PlanHeaderInfoTableViewCell.swift

--- Sources/PMPComponents/PlanHeaderInfoTableViewCell.swift
+++ Sources/PMPComponents/PlanHeaderInfoTableViewCell.swift
@@ -52,7 +52,7 @@ public class PlanHeaderInfoTableViewCell: UITableViewCell, TableViewCellSerializ
         div2.height(1)
 
         let stackView = IOComponent.createStackView(axisType: .vertical, list: [label, div2, sublabel])
-        stackView.spacing = 10
+        stackView.spacing = 2
         stackView.alignment = .leading
         stackView.distribution = .fillProportionally
         label.numberOfLines = 0
@@ -66,7 +66,7 @@ public class PlanHeaderInfoTableViewCell: UITableViewCell, TableViewCellSerializ
         layout {
             10
             |-20-stackView-20-|
-            4
+            10
             |-20-div1-20-|
             10
         }
